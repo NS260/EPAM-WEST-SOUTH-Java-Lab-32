f81f9324ae3f42dedca224dfceb27e2a67439053
Nazar Slobodeniuk <97839433+NS260@users.noreply.github.com>
2022-06-29 21:25:03 +0300

    feat: Added configurations[BeansConfig,OtherConfig]

---
+++ src/main/java/com/epam/spring/homework1/config/OtherConfig.java
@@ -0,0 +1,10 @@
+package com.epam.spring.homework1.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+@ComponentScan(basePackages = "com.epam.spring.homework1.other")
+@Configuration
+public class OtherConfig {
+}
